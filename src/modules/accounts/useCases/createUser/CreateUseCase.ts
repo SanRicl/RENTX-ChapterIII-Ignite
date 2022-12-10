b9238e7acc512916a3ddbc81d4e01441d0f493e2
sandersonricl <sandersonricl@gmail.com>
2022-12-10 16:21:36 -0300

    FIX: verifying if users exists

--- src/modules/accounts/useCases/createUser/CreateUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import { hash } from 'bcrypt';
 import { ICreateUserDTO } from '../../dtos/ICreateUsersDTO';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
@@ -15,11 +16,15 @@ class CreateUserUseCase {
     name,
     password,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.userRepository.findByEmail(email);
+    if (userAlreadyExists) throw new Error('User already exists');
+
+    const passwordHash = await hash(password, 8);
     await this.userRepository.create({
       driver_license,
       email,
       name,
-      password,
+      password: passwordHash,
     });
   }
 }
